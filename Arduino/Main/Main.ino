fe99bf87fa212792540c3acbb2e52b6c64c4c843
An GyuBin <bkgr4949p@icloud.com>
2018-10-25 15:44:28 +0900

    Undating Arduino Source

--- Arduino/Main/Main.ino
+++ Arduino/Main/Main.ino
@@ -2,20 +2,19 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
 }
 
+String cmd;
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()){
     char c = Serial.read();
-    Serial.write(c);
-    Serial.println("");
-    if(c == 'O')
-      Serial.println("CMD TLT OPEN 1");
-    else if(c == 'C')
-      Serial.println("CMD TLT CLOSE 1");
-      
-    Serial.println("");
+    if(c == '@'){
+      Serial.println(cmd);
+      cmd = "";
+    }
+    else{
+      cmd += c;
+    } 
   }
 }
